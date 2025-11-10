e4640079714b3ecf26e22eef874a323dbd11da01
dgyc999 <134274850+yaliyang0102@users.noreply.github.com>
2025-11-11 02:25:00 +0800

    Update game-client.tsx

--- src/app/game-client.tsx
+++ src/app/game-client.tsx
@@ -1,8 +1,7 @@
 // src/app/game-client.tsx
-"use client";
-
-import { Providers } from "./providers";
-import GameBoard from "../components/GameBoard";
+'use client';
+import { Providers } from './providers';
+import GameBoard from '../components/GameBoard';
 
 export default function GameClient() {
   return (
