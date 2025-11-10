53ca3e1bbb9633c5bf255fbc48c882c34e612930
dgyc999 <134274850+yaliyang0102@users.noreply.github.com>
2025-11-10 23:20:02 +0800

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,15 @@
 "use client";
 
+import { ReactNode, useState } from "react";
 import { WagmiProvider } from "wagmi";
 import { wagmiConfig } from "../wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+export function Providers({ children }: { children: ReactNode }) {
+  const [qc] = useState(() => new QueryClient());
   return (
     <WagmiProvider config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={qc}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
 }
