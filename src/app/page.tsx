613d61e396eeab91cfcd1baa0ef028dff812842c
dgyc999 <134274850+yaliyang0102@users.noreply.github.com>
2025-11-11 02:24:42 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-import NextDynamic from 'next/dynamic'; // ✅ 改名，避免与上面的 dynamic 冲突
-
+import NextDynamic from 'next/dynamic';
 const GameClient = NextDynamic(() => import('./game-client'), { ssr: false });
 
 export default function Page() {
